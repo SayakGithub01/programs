38093b522645cda014f2726dee2f0df193b99f72
SayakGithub01 <sayak.majumdarmca@gmail.com>
2021-04-11 21:50:59 +0530

    files added

---
+++ first.java
@@ -0,0 +1,8 @@
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("+-*/+sayakmajumdar+-sayakl00");
+        System.out.println("https://github.com/SayakGithub01/MavenBuild.git");
+    }
+}
