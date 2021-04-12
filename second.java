b2bde6dd0f725c0f6ab6efcacc4f53c2b4fe5047
SayakGithub01 <sayak.majumdarmca@gmail.com>
2021-04-12 15:15:22 +0530

    testcommit

---
+++ second.java
@@ -0,0 +1,8 @@
+public class second
+{
+    public static void main(String[] args)
+    {
+        System.out.println("sayakmajumdar*****Tests");
+        System.out.println("https://github.com/SayakGithub01/MavenBuild.git");
+    }
+}
