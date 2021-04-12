394ab1435009e39bc356003b20d3390b28d30838
SayakGithub01 <sayak.majumdarmca@gmail.com>
2021-04-12 15:06:23 +0530

    changes made

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ public class first
 {
     public static void main(String[] args)
     {
-        System.out.println("sayakmajumdar*****");
+        System.out.println("sayakmajumdar*****Tests");
         System.out.println("https://github.com/SayakGithub01/MavenBuild.git");
     }
 }
